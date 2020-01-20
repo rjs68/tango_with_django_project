f13fd9b8d050b26ec438e807b5ea82759119f149
Rhys <rhys-stephens@hotmail.co.uk>
2020-01-20 16:03:30 +0000

    Create basic html template for pages and added ability to handle static files

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
+    # Construct a dictionary to pass to the template engine
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return a rendered response to send to the client
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
