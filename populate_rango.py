9ae43dbd3b8e55c6cbb09271bb4edb8cda08423e
Rhys <rhys-stephens@hotmail.co.uk>
2020-02-01 15:24:37 +0000

    Added ability to serve dynamic content from the database

--- populate_rango.py
+++ populate_rango.py
@@ -10,27 +10,27 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url': 'http://docs.python.org/3/tutorial/'},
+        'url': 'http://docs.python.org/3/tutorial/', 'views': 19},
         {'title': 'How to Think like a Computer Scientist',
-        'url': 'http://www.greenteapress.com/thinkpython/'},
+        'url': 'http://www.greenteapress.com/thinkpython/', 'views': 19},
         {'title': 'Learn Python in 10 Minutes',
-        'url': 'http://www.korokithakis.net/tutorials/python/'}
+        'url': 'http://www.korokithakis.net/tutorials/python/', 'views': 19}
     ]
 
     django_pages = [
         {'title': 'Official Django Tutorial',
-        'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': 19},
         {'title': 'Django Rocks',
-        'url': 'http://www.djangorocks.com/'},
+        'url': 'http://www.djangorocks.com/', 'views': 19},
         {'title': 'How to Tango with Django',
-        'url': 'http://www.tangowithdjango.com/'}
+        'url': 'http://www.tangowithdjango.com/', 'views': 19}
     ]
 
     other_pages = [
         {'title': 'Bottle',
-        'url': 'http://bottlepy.org/docs/dev/'},
+        'url': 'http://bottlepy.org/docs/dev/', 'views': 19},
         {'title': 'Flask',
-        'url': 'http://flask.pocoo.org'}
+        'url': 'http://flask.pocoo.org', 'views': 19}
     ]
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
@@ -43,7 +43,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     #print out the added categories
     for c in Category.objects.all():
